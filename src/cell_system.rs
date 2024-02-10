43ca248959deca32698f50bbd9e9a65af306b78c
Aaron <aaronkanaron@gmail.com>
2024-02-10 23:29:35 +0100

    colors, better placement

--- src/cell_system.rs
+++ src/cell_system.rs
@@ -2,14 +2,14 @@
 
 
 /*- Imports -*/
-use std::{cell, time::Duration};
+use std::time::Duration;
 use rand::Rng;
 use bevy::{prelude::*, window::PrimaryWindow};
 
 use crate::{X_MAX_BOUNDS, X_MIN_BOUNDS, Y_MAX_BOUNDS, Y_MIN_BOUNDS};
 
 /*- Constants -*/
-static NEIGHBOR_DELTA: [(isize, isize); 2] = [(-1, -1), (1, -1)];
+const PI: f64 = 3.14159265359;
 
 /*- Structs, enums & unions -*/
 #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
@@ -46,95 +46,20 @@ impl Plugin for CellSystem {
     fn build(&self, app: &mut App) {
         let cell_params = CellParams::default();
         app.insert_resource(cell_params)
-            .insert_resource(NextGenerationTimer(Timer::new(Duration::from_millis(10), TimerMode::Repeating)))
+            .insert_resource(NextGenerationTimer(Timer::new(Duration::from_millis(1), TimerMode::Repeating)))
             .add_systems(Update, system_handle_mouse)
-            .add_systems(Startup, init_cells.in_set(CellSet))
+            // .add_systems(Startup, init_cells.in_set(CellSet))
             .add_systems(Update, system_cells.in_set(CellSet));
     }
 }
 
 /*- Initialize -*/
-fn init_cells(mut commands: Commands) {
-    // commands.spawn(CellPosition { x: 0, y: 0, static_cell: false});
-    // commands.spawn(CellPosition { x: 0, y: 12, static_cell: false});
-    // commands.spawn(CellPosition { x: 0, y: -20, static_cell: true});
-}
-
-// fn system_cells(
-//     mut commands: Commands,
-//     q_cells: Query<(Entity, &CellPosition)>,
-//     mut timer: ResMut<NextGenerationTimer>,
-//     mut cell_params: ResMut<CellParams>,
-//     time: Res<Time>,
-// ) {
-//     // Run next generation if the timer is finished.
-//     if cell_params.playing {
-//         timer.0.tick(time.delta());
-//         if timer.0.finished() { cell_params.compute_next_gen = true }
-//     }
-//     if cell_params.compute_next_gen { cell_params.compute_next_gen = false }
-//     else { return }
-
-//     for (entity, cell_position) in q_cells.iter() {
-//         if cell_position.static_cell { continue; }         // We do not want to iterate over static cells
-
-//         let scan_pos_below = CellPosition {
-//             x: cell_position.x,
-//             y: cell_position.y - 1,
-//             static_cell: true,
-//         };
-
-//         let scan_pos_right = CellPosition {
-//             x: cell_position.x + 1,
-//             y: cell_position.y,
-//             static_cell: true,
-//         };
-
-//         let scan_pos_left = CellPosition {
-//             x: cell_position.x - 1,
-//             y: cell_position.y,
-//             static_cell: true,
-//         };
-
-//         //check if any cell is equal to the scan position scan pos below
-//         if !q_cells.iter().any(|(_, pos)| pos == &scan_pos_below) {
-//             if scan_pos_below.y <= Y_MIN_BOUNDS || scan_pos_below.y >= Y_MAX_BOUNDS || scan_pos_below.x <= X_MIN_BOUNDS || scan_pos_below.x >= X_MAX_BOUNDS {
-
-//                 continue;
-//             }
-//             /*
-//             ! Lots of warnings from this code when spawning entities,
-//             ! I think it has to do with the fact that the cells gets layered
-//             ! on top of each other and it cannot despawn the entity because it already got despawned by the cell below it.
-//             */
-//             commands.spawn(scan_pos);
-//             commands.entity(entity).despawn();
-//         } else {
-//             //Check left and right and move if possible
-//             for position_delta in NEIGHBOR_DELTA.iter() {
-//                 let scan_pos = CellPosition {
-//                     x: cell.x + position_delta.0,
-//                     y: cell.y + position_delta.1,
-//                 };
-//                 // ! PRETTY SURE THIS DOES THE RIGHT POSITION FIRST AND THEN THE LEFT IT SHOULD BE RANDOM
-//                 // ! Om man skapar en pyramid, och sedan placerar en cell högst upp blir det en cell till höger och en cell till vänster
-//                 // ! En bättre lösning hade varit att slumpa vilken sida som cellen hamnar på.
-//                 // ! Anledningen till att den varnar då är på grund av att den försöker
-//                 // ! Despawna en cell som inte längre existerar, eftersom att den blev bortagen av en utav de två cellerna som cellen splittades till.
-//                 if !q_cells.iter().any(|(_, pos)| pos == &scan_pos) { 
-//                     if scan_pos.x <= X_MIN_BOUNDS || scan_pos.x >= X_MAX_BOUNDS || scan_pos.y <= Y_MIN_BOUNDS || scan_pos.y >= Y_MAX_BOUNDS {
-//                         // println!("{:?} is at the edge", scan_pos);
-//                         continue;
-//                     } 
-//                     commands.entity(entity).despawn();
-//                     commands.spawn(scan_pos);
-//                     // println!("Moved cell to {:?} | {:?}", scan_pos, new_cell);
-//                     // break;
-//                 }
-//             }
-//         }
-//     }
+// fn init_cells(mut commands: Commands) {
+//     // commands.spawn(CellPosition { x: 0, y: 0, static_cell: false});
+//     // commands.spawn(CellPosition { x: 0, y: 12, static_cell: false});
+//     // commands.spawn(CellPosition { x: 0, y: -20, static_cell: true});
 // }
+
 fn system_cells(
     // mut commands: Commands,
     mut q_cells: Query<(Entity, &mut CellPosition)>,
@@ -163,7 +88,8 @@ fn system_cells(
         let scan_pos_right = (cell_position.x + 1, cell_position.y - 1);
         let scan_pos_left = (cell_position.x - 1, cell_position.y - 1);
 
-        if scan_pos_below.1 <= Y_MIN_BOUNDS || scan_pos_below.1 >= Y_MAX_BOUNDS || scan_pos_below.0 <= X_MIN_BOUNDS || scan_pos_below.0 >= X_MAX_BOUNDS {
+        // Skip out of bounds cells and mark them as static since they can't move
+        if scan_pos_below.1 < Y_MIN_BOUNDS || scan_pos_below.1 > Y_MAX_BOUNDS || scan_pos_below.0 < X_MIN_BOUNDS || scan_pos_below.0 > X_MAX_BOUNDS {
             cell_position.static_cell = true;
             continue;
         }
@@ -173,7 +99,7 @@ fn system_cells(
         } else if !static_cell_positions.contains(&scan_pos_right) && !static_cell_positions.contains(&scan_pos_left) {
             let mut rng = rand::thread_rng();
 
-            //generate random boolean
+            //if boolean true, move right, else move left
             if rng.gen() {
                 cell_position.x += 1;
                 cell_position.y -= 1;
@@ -201,7 +127,9 @@ fn system_handle_mouse(
     mouse_button: Res<Input<MouseButton>>,
     q_windows: Query<&Window, With<PrimaryWindow>>,
     q_camera: Query<(&Camera, &GlobalTransform)>,
+    q_cells: Query<(Entity, &CellPosition)>
 ) {
+    let ball_size = 4;
     if !mouse_button.pressed(MouseButton::Left) { return }
     
     // Get the cursor position
@@ -218,12 +146,34 @@ fn system_handle_mouse(
         return;
     }
 
-    let new_cell = CellPosition {
-        x: target_pos.x as isize,
-        y: target_pos.y as isize,
-        static_cell: false,
-    };
+    let deltas = vec![(0, 0), (0, 1), (1, 0), (1, 1)];
 
-    commands.spawn(new_cell);
+    for (dx, dy) in deltas {
+        let new_cell = CellPosition {
+            x: (target_pos.x + dx as f32) as isize,
+            y: (target_pos.y + dy as f32) as isize,
+            static_cell: false,
+        };
+
+        //check if there is already a cell at the position
+        if q_cells.iter().any(|(_, cell_position)| cell_position.x == new_cell.x && cell_position.y == new_cell.y) {
+            continue;
+        }
+
+        //check if outside boundaries
+        if new_cell.x < X_MIN_BOUNDS || new_cell.x > X_MAX_BOUNDS || new_cell.y < Y_MIN_BOUNDS || new_cell.y > Y_MAX_BOUNDS {
+            continue;
+        }
+
+        commands.spawn(new_cell);
+    }
+
+    // let new_cell = CellPosition {
+    //     x: target_pos.x as isize,
+    //     y: target_pos.y as isize,
+    //     static_cell: false,
+    // };
+
+    // commands.spawn(new_cell);
 }
 
--- src/gui.rs
+++ src/gui.rs
@@ -7,16 +7,20 @@ use crate::cell_system::{CellPosition, CellSet};
 
 /*- Constants -*/
 const BACKGROUND_COLOR: Color = Color::rgb(0.08, 0.08, 0.08);
-const SCALE_DEF: f32 = 1. / 4.;
-const SCALE_MAX: f32 = 1.;
+const SCALE_DEF: f32 = 1. / 4.; // zoom
+// const SCALE_MAX: f32 = 1.;
 
 /*- Structs, enums & unions -*/
 pub struct GuiSystem;
+
+#[derive(Component)]
+struct EntityCounter;
 
 impl Plugin for GuiSystem {
     fn build(&self, app: &mut App) {
         app.insert_resource(ClearColor(BACKGROUND_COLOR))
             .add_systems(Startup, init_camera)
+            // .add_systems(Update, system_entity_counter)
             .add_systems(Update, system_draw_new_cells.before(CellSet));
     }
 }
@@ -32,6 +36,7 @@ fn init_camera(mut commands: Commands) {
     commands.spawn(camera);
 }
 
+
 /*- Update Systems -*/
 fn system_draw_new_cells(
     mut commands: Commands,
@@ -42,7 +47,7 @@ fn system_draw_new_cells(
         commands.entity(entity)
             .insert(SpriteBundle {
                 sprite: Sprite {
-                    color: Color::GREEN,
+                    color: color_code(position.x, position.y),
                     custom_size: Some(Vec2::new(1., 1.)),
                     ..Default::default()
                 },
@@ -52,5 +57,20 @@ fn system_draw_new_cells(
     }
 }
 
+fn system_entity_counter(
+    mut commands: Commands,
+    q_cells: Query<&CellPosition>
+) {
+    let mut counter = 0;
+    for _ in q_cells.iter() {
+        counter += 1;
+    }
+    println!("Entity count: {}", counter);
+}
+
 
 /*- Method implementations - */
+fn color_code(x: isize, y: isize) -> Color {
+    if x % 2 == 0 { if y % 2 == 0 { Color::rgb(0.5, 0.5, 0.5) } else { Color::rgb(0.8, 0.8, 0.8) } }
+    else { if y % 2 == 0 { Color::rgb(0.8, 0.8, 0.8) } else { Color::rgb(0.5, 0.5, 0.5) } }
+}
